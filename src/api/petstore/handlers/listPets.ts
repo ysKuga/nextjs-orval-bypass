f944e87596879ec73a232e1275d085086817ae4a
ysKuga <horrormovies.noga@gmail.com>
2025-10-03 08:56:57 +0900

    #3 feat: レスポンスに mock 適用

--- src/api/petstore/handlers/listPets.ts
+++ src/api/petstore/handlers/listPets.ts
@@ -1,6 +1,7 @@
 import { createFactory } from 'hono/factory';
 
 import { ListPetsContext } from '../generated/index.context';
+import { getListPetsResponseMock } from '../generated/index.msw';
 import { zValidator } from '../generated/index.validator';
 import { listPetsQueryParams, listPetsResponse } from '../generated/index.zod';
 
@@ -9,5 +10,9 @@ const factory = createFactory();
 export const listPetsHandlers = factory.createHandlers(
   zValidator('query', listPetsQueryParams),
   zValidator('response', listPetsResponse),
-  async (c: ListPetsContext) => {},
+  async (c: ListPetsContext) => {
+    return c.body(
+      Array.from({ length: 3 }).map(() => getListPetsResponseMock()),
+    );
+  },
 );
